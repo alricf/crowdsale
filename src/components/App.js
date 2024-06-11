0eaa61526284f56d291e392df8d417721561e2fe
alricf <alricfernandes11@gmail.com>
2024-06-11 11:15:35 -0400

    Adds Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-const message = 'Hello, world!';
+import { Container } from 'react-bootstrap';
+
+// Components
+import Navigation from './Navigation';
 
 function App() {
-
   return (
-    <>
-    </>
+    <Container>
+      <Navigation />
+    </Container>
   );
 }
 
