04deb3b277bd084c8c8a6e1bf8b074004e40d0bd
alricf <alricfernandes11@gmail.com>
2024-06-13 11:21:05 -0400

    Adds front end

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { ethers } from 'ethers';
 
 // Components
 import Navigation from './Navigation';
+import Buy from './Buy';
 import Info from './Info';
 import Progress from './Progress';
 import Loading from './Loading';
@@ -83,6 +84,7 @@ function App() {
       ) : (
         <>
           <p className='text-center'><strong>Current Price:</strong> {price} ETH</p>
+          <Buy provider={provider} price={price} crowdsale={crowdsale} setIsLoading={setIsLoading} />
           <Progress maxTokens={maxTokens} tokensSold={tokensSold} />
         </>
       )}
